1f2d55535e2f4d96ff5d9656898e57e72851dde2
Spyridon Paraschos <spgen20@live.com>
2020-05-28 01:18:33 +0300

    changes to Multi-Label

--- multilabel.py
+++ multilabel.py
@@ -3,8 +3,9 @@ import numpy as np
 
 def BinaryRelevance(X,y):
     Y = np.transpose(y)
+    #labels = Y.copy()
     for e,t in enumerate(Y):
-        t[t>0]=e+1
+        #t[t>0]=e+1
         yield X,t
     
 def CalibratedLabelRanking(X,y):
@@ -12,22 +13,22 @@ def CalibratedLabelRanking(X,y):
     indices, pairs = [],[]
     for e1,i in enumerate(Y):
         for e2,j in enumerate(Y[e1+1:]):
-            index1, pair1 = [],[]
+            index, pair = [],[]
             for x in range(len(i)):
                 if i[x] == j[x]:
                     continue
                 else:
-                    pair1.append(i[x]*(e1+1)+j[x]*(e1+e2+2))
-                    index1.append(x)
-            pairs.append(pair1)
-            indices.append(index1)
+                    pair.append(i[x]*(e1+1)+j[x]*(e1+e2+2))
+                    index.append(x)
+            pairs.append(pair[:])
+            indices.append(index[:])
             
-        index2, pair2 = [],[]
+        index, pair = [],[]
         for e,x in enumerate(i):
-            pair2.append(x*(e1+1))
-            index2.append(e)
-        pairs.append(pair2)
-        indices.append(index2)
+            pair.append(x*(e1+1))
+            index.append(e)
+        pairs.append(pair[:])
+        indices.append(index[:])
         
     for e,t in enumerate(pairs):
         data = X[np.array(indices[e])]
