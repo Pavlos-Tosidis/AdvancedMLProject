6573fd71caf62080b04f87f1c1ad4408c6350f94
apournaras <alexpour@csd.auth.gr>
2020-05-31 16:45:22 +0300

    added tomek links and smote in class imbalance techniques

--- classimbalance.py
+++ classimbalance.py
@@ -1,5 +1,7 @@
 from collections import Counter
 from imblearn.under_sampling import RandomUnderSampler
+from imblearn.under_sampling import TomekLinks
+from imblearn.over_sampling import SMOTE
 
 
 def random_undersampler(X, y):
@@ -7,3 +9,21 @@ def random_undersampler(X, y):
     rus = RandomUnderSampler(random_state=0)
     X_resampled, y_resampled = rus.fit_resample(X, y)
     print(sorted(Counter(y_resampled).items()))
+    return X_resampled, y_resampled
+
+
+# a lot slower than random undersampling and SMOTE
+def tomek_links(X,y):
+    print(sorted(Counter(y).items()))
+    tml = TomekLinks()
+    X_resampled, y_resampled = tml.fit_resample(X, y)
+    print(sorted(Counter(y_resampled).items()))
+    return X_resampled, y_resampled
+    
+    
+def smote(X,y):
+    print(sorted(Counter(y).items()))
+    smo = SMOTE()
+    X_resampled, y_resampled = smo.fit_resample(X, y)
+    print(sorted(Counter(y_resampled).items()))
+    return X_resampled, y_resampled
